0ad8260cfa8799ea4397eaf87187627e976e68ca
Jonathan Luembe <jonathanluembe@yahoo.com>
2025-06-10 16:41:53 +0200

    Design frontpage

--- frontend/src/pages/Acceuil.jsx
+++ frontend/src/pages/Acceuil.jsx
@@ -5,15 +5,16 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import BanniereVideo from '../assets/Bannière_vidéo.mp4';
 import banniere from '../assets/banniere.png'
-import BannierConnecte from '../assets/connexion-reseau.png';
+import BannierConnecte from '../assets/Carte.png';
 import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
 
 
 const Acceuil = () => {
   return (
     <div className="font-sans antialiased">
       {/* Hero Section */}
-      <section className="relative min-h-[400px] md:min-h-[630px] flex items-center justify-center overflow-hidden">
+      <section className="relative min-h-[400px] md:min-h-[630px] flex items-center justify-start overflow-hidden">
         {/* Video Background */}
         <video
           autoPlay
@@ -28,8 +29,8 @@ const Acceuil = () => {
         {/* Overlay */}
         <div className="absolute z-20 inset-0 bg-black opacity-50"></div>
         {/* Content */}
-        <div className="relative z-30 text-white text-center py-20 px-4">
-          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4">Bienvenue à<br />ICC ONLINE.</h1>
+        <div className="relative z-30 text-white text-left py-20 pl-20 pr-4">
+          <h1 className="text-2xl sm:text-4xl md:text-8xl font-bold mb-4">Bienvenue à<br />ICC ONLINE.</h1>
           <p className="text-base sm:text-lg md:text-xl">
             Découvrez notre vision, nos<br />programmes en rejoignant une<br />communauté dynamique et engagée.
           </p>
@@ -38,8 +39,8 @@ const Acceuil = () => {
 
       {/* Connected Family Section */}
       <section className="flex flex-col md:flex-row items-center justify-center py-16 px-4 bg-gray-100">
-        <div className="w-full flex flex-col items-center md:w-1/2 text-left px-20 mb-8 md:mb-0 md:pr-8">
-          <h2 className="text-3xl font-bold mb-4">Une famille connectée</h2>
+        <div className="w-full flex flex-col md:w-1/2 text-left px-20 mb-8 md:mb-0 md:pr-8">
+          <h2 className="text-3xl font-bold mb-4 text-left">Une famille connectée</h2>
           <p className="text-gray-700 mb-4">
             Impact Centre Chrétien en ligne, est la dimension digitale des églises<br />Impact Centre Chrétien.<br />Campus à part entière, ICC ONLINE regroupe toutes les personnes<br />désirant appartenir à la grande famille Impact Centre Chrétien<br />mais n'ayant pas de campus physique à proximité de chez elles.
           </p>
@@ -49,16 +50,16 @@ const Acceuil = () => {
         </div>
         <div className="md:w-1/2 flex justify-center">
           {/* Illustration Placeholder */}
-          <div className="w-64 h-64  rounded-full flex items-center justify-center text-gray-600">
-            <img src={BannierConnecte} alt="Illustration" className="w-48 h-48 sm:w-64 sm:h-64 object-contain" />
+          <div className="  rounded-full flex items-center justify-center text-gray-600">
+            <img src={BannierConnecte} alt="Illustration" className=" object-contain" />
           </div>
         </div>
       </section>
 
-      {/* Join Online Impact Family Section */}<section className="relative w-full min-h-[400px] md:min-h-[630px] flex items-center justify-center text-white px-4 overflow-hidden">
+      {/* Join Online Impact Family Section */}<section className="relative w-full min-h-[400px] md:min-h-[630px] flex items-center justify-start text-white px-4 overflow-hidden">
         <img src={banniere} alt="Bannière" className="absolute inset-0 w-full h-full object-cover" />
         <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="relative z-10 w-full max-w-3xl text-center md:text-left px-4">
+        <div className="relative z-10 w-full max-w-3xl flex flex-col justify-start md:text-left pl-20 pr-4">
           <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4">
             Rejoignez une famille<br />d'impact online
           </h2>
