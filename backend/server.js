aaa7ab90cd9ac1cd4ce1ab168f63a34b0e525160
Jonathan Luembe <jonathanluembe@yahoo.com>
2025-07-17 15:05:13 +0200

    Server update

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ const { Server } = require('socket.io');
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: ['http://localhost:5173', ], credentials: true }
+  cors: { origin: ['http://localhost:5173',
+    'https://r-ss.vercel.app'
+   ], credentials: true }
 });
 
 // Stockage des sockets par userId
